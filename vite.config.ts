5ead5ff0e14cdf58aab55812e4a8bc2c33a23e7b
Streptospondylus <campostheo@outlook.com>
2026-09-08 15:37:13 +0200

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/ratoncito-clinical/",
   build: {
     target: "es2020",
     sourcemap: false
